62b612ebf8878b8a47bd7956dfe9cbdd0ce7082b
Tom-Jank <tjank@deloitte.com>
2022-07-21 13:34:12 +0200

    Deleted exception in SimulationStarter.java

--- src/main/java/simulation/SimulationStarter.java
+++ src/main/java/simulation/SimulationStarter.java
@@ -3,10 +3,8 @@ This class is responsible for running the simulation.
  */
 package main.java.simulation;
 
-import java.io.IOException;
-
 public class SimulationStarter {
-    public static void runningSimulation() throws IOException {
+    public static void runningSimulation() {
         GameLogic game = new GameLogic();
         for (int i=0; i<=30; i++) {
             game.game();
